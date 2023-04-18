8806f43541460df01156636cd6a7a1cb169daccc
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2023-04-18 14:23:44 +0300

    Bug fix Update producer batch size

--- JeMPI_Apps/JeMPI_AsyncReceiver/src/main/java/org/jembi/jempi/async_receiver/CustomMain.java
+++ JeMPI_Apps/JeMPI_AsyncReceiver/src/main/java/org/jembi/jempi/async_receiver/CustomMain.java
@@ -130,14 +130,15 @@ public final class CustomMain {
                                            batchMetaData,
                                            null));
          for (CSVRecord csvRecord : csvParser) {
-            final var patientPk = csvRecord.get(0);
-            final var siteCode = csvRecord.get(1);
-
+            final var patientPkv = (csvRecord.get(0) == "NULL") ? null : csvRecord.get(0);
+            final var siteCode = (csvRecord.get(1) == "NULL") ? null : csvRecord.get(1);
+            final var patientPk = (csvRecord.get(2) == "NULL") ? null : csvRecord.get(2);
+            final var nupi = (csvRecord.get(3) == "NULL") ? null : csvRecord.get(3);
             var recordKey = uuid;
             if (patientPk != null && siteCode != null) {
                recordKey = patientPk.concat(siteCode);
             }
-            final var dwhId = dwh.insertClinicalData(patientPk, siteCode, csvRecord.get(2), csvRecord.get(3));
+            final var dwhId = dwh.insertClinicalData(patientPkv, siteCode, patientPk, nupi);
             sendToKafka(recordKey,
                         new AsyncSourceRecord(AsyncSourceRecord.RecordType.BATCH_RECORD,
                                               batchMetaData,
@@ -146,10 +147,10 @@ public final class CustomMain {
                                                     new SourceId(null, csvRecord.get(1), csvRecord.get(0)),
                                                     null,
                                                     dwhId,
-                                                    patientPk,
+                                                    patientPkv,
                                                     siteCode,
-                                                    csvRecord.get(2),
-                                                    csvRecord.get(3)
+                                                    patientPk,
+                                                    nupi
                                                     )));
          }
          sendToKafka(uuid,
