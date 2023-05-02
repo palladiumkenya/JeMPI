c1a499e6c36bed6c8afaeac487b4b91441b1acaa
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2023-05-02 08:42:32 +0300

    Remove drop schema on linker start

--- JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/BackEnd.java
+++ JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/BackEnd.java
@@ -123,9 +123,9 @@ public final class BackEnd extends AbstractBehavior<BackEnd.Event> {
       final var port = new int[]{AppConfig.DGRAPH_ALPHA1_PORT}; //, AppConfig.DGRAPH_ALPHA2_PORT, AppConfig.DGRAPH_ALPHA3_PORT};
       libMPI = new LibMPI(host, port);
       libMPI.startTransaction();
-      if (!(libMPI.dropAll().isEmpty() && libMPI.createSchema().isEmpty())) {
-         LOGGER.error("Create Schema Error");
-      }
+      // if (!(libMPI.dropAll().isEmpty() && libMPI.createSchema().isEmpty())) {
+      //    LOGGER.error("Create Schema Error");
+      // }
       libMPI.closeTransaction();
    }
 
