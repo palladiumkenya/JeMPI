67807865ce6f0d05164143436174d06a62aec2aa
Paul Nthusi <pknthusi@gmail.com>
2023-07-31 00:10:31 +0300

    Update Kenya config.

--- JeMPI_Apps/JeMPI_AsyncReceiver/src/main/java/org/jembi/jempi/async_receiver/CustomAsyncHelper.java
+++ JeMPI_Apps/JeMPI_AsyncReceiver/src/main/java/org/jembi/jempi/async_receiver/CustomAsyncHelper.java
@@ -4,35 +4,38 @@ import org.apache.commons.csv.CSVRecord;
 import org.jembi.jempi.shared.models.CustomDemographicData;
 import org.jembi.jempi.shared.models.CustomSourceId;
 import org.jembi.jempi.shared.models.CustomUniqueInteractionData;
+import scala.Tuple2;
 
 final class CustomAsyncHelper {
 
-   private static final int AUX_ID_COL_NUM = 0;
-   private static final int CCC_NUMBER_COL_NUM = 8;
-   private static final int PKV_COL_NUM = 9;
-   private static final int SOURCEID_FACILITY_COL_NUM = 6;
-   private static final int SOURCEID_PATIENT_COL_NUM = 7;
-   private static final int GIVEN_NAME_COL_NUM = 1;
-   private static final int FAMILY_NAME_COL_NUM = 2;
-   private static final int GENDER_COL_NUM = 3;
-   private static final int DOB_COL_NUM = 4;
-   private static final int NUPI_COL_NUM = 5;
+   private static final int CCC_NUMBER_COL_NUM = 6;
+   private static final int PKV_COL_NUM = 0;
+   private static final int SOURCEID_FACILITY_COL_NUM = 4;
+   private static final int SOURCEID_PATIENT_COL_NUM = 5;
+//   private static final int GIVEN_NAME_COL_NUM = 0;
+//   private static final int FAMILY_NAME_COL_NUM = 0;
+   private static final int GENDER_COL_NUM = 1;
+   private static final int DOB_COL_NUM = 2;
+   private static final int NUPI_COL_NUM = 3;
 
    private CustomAsyncHelper() {
    }
 
    static CustomUniqueInteractionData customUniqueInteractionData(final CSVRecord csvRecord, final String dwhId) {
       return new CustomUniqueInteractionData(java.time.LocalDateTime.now(),
-                                             Main.parseRecordNumber(csvRecord.get(AUX_ID_COL_NUM)),
+                                             null,
                                              csvRecord.get(CCC_NUMBER_COL_NUM),
                                              csvRecord.get(PKV_COL_NUM),
                                              dwhId);
    }
 
    static CustomDemographicData customDemographicData(final CSVRecord csvRecord) {
+      Tuple2<String, String> phoneticNames =  CustomInputHelper.parsePkv(csvRecord.get(PKV_COL_NUM));
+      String givenNameSoundex = phoneticNames == null ? "" : phoneticNames._1();
+      String familyNameDoubleMetaphone = phoneticNames == null ? "" : phoneticNames._2();
       return new CustomDemographicData(
-         csvRecord.get(GIVEN_NAME_COL_NUM),
-         csvRecord.get(FAMILY_NAME_COL_NUM),
+         givenNameSoundex,
+         familyNameDoubleMetaphone,
          csvRecord.get(GENDER_COL_NUM),
          csvRecord.get(DOB_COL_NUM),
          csvRecord.get(NUPI_COL_NUM));
