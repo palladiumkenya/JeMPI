8ab6ff861e258e8902b0ee1f920db9cf1ed68bad
Paul Nthusi <pknthusi@gmail.com>
2023-08-19 00:50:29 +0300

    Scale up linker Switch match config

--- JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/backend/CustomLinkerDeterministic.java
+++ JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/backend/CustomLinkerDeterministic.java
@@ -20,7 +20,18 @@ final class CustomLinkerDeterministic {
          final CustomDemographicData interaction) {
       final var nupiL = goldenRecord.nupi;
       final var nupiR = interaction.nupi;
-      return isMatch(nupiL, nupiR);
+      if (isMatch(nupiL, nupiR)) {
+         return true;
+      }
+      final var givenNameL = goldenRecord.givenName;
+      final var givenNameR = interaction.givenName;
+      final var familyNameL = goldenRecord.familyName;
+      final var familyNameR = interaction.familyName;
+      final var genderL = goldenRecord.gender;
+      final var genderR = interaction.gender;
+      final var dobL = goldenRecord.dob;
+      final var dobR = interaction.dob;
+      return (isMatch(givenNameL, givenNameR) && isMatch(familyNameL, familyNameR) && isMatch(genderL, genderR) && isMatch(dobL, dobR));
    }
 
 }
