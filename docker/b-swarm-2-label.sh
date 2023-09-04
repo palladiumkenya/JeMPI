0fa93f2e73486b9a1fbbd71c854912aff1e975e8
Paul Nthusi <pknthusi@gmail.com>
2023-09-04 09:48:38 +0300

    Update swarm node labels

--- docker/b-swarm-2-label.sh
+++ docker/b-swarm-2-label.sh
@@ -5,6 +5,6 @@ set -m
 
 source ./0-conf.env
 
-docker node update --label-add name=$NODE3 $NODE3
+# docker node update --label-add name=$NODE3 $NODE3
 docker node update --label-add name=$NODE2 $NODE2
 docker node update --label-add name=$NODE1 $NODE1
