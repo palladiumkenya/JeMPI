47a63515acb3a3ad4ed016a6d7f5ea6503eb6ce2
Paul Nthusi <pknthusi@gmail.com>
2024-05-22 20:53:07 +0300

    Cleanup

--- JeMPI_Apps/JeMPI_ETL/src/main/java/org/jembi/jempi/etl/ETLUtil.java
+++ JeMPI_Apps/JeMPI_ETL/src/main/java/org/jembi/jempi/etl/ETLUtil.java
@@ -1,7 +1,5 @@
 package org.jembi.jempi.etl;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jembi.jempi.shared.models.CustomDemographicData;
 import org.jembi.jempi.shared.models.CustomUniqueInteractionData;
 import scala.Tuple2;
